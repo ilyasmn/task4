c474b4491d124587691aa50c4e68e65fac42e8c0
ilyasmn <ilyasnm@code.edu.az>
2022-04-11 23:50:22 +0400

    ok

--- sual 4/Program.cs
+++ sual 4/Program.cs
@@ -5,74 +5,40 @@ namespace sual_4
     internal class Program
     {
         static void Main(string[] args)
-
-
-
-           
-            //11-e bolunub  11-ci eded hansidir?
         {
-            
+            int count = 0; int qaliq = 0; 
             for (int i = 1; i < 1000; i++)
-            {
-
-                int a = i;
-                int f=a;
-                int g;
-                int h = a;
-                int sum= 0;
-                int b;
-                int z;
-                int sum2 = 0;
-                bool state = false;
-                bool state2 = false;
-               int count = 0;
-
-
-
-                while (a > 0)
-                {
-                    b = a % 10;
-                    sum += b;
-                   a /=10;
-
-                    if (sum>11)
-                    {
-                        state = true;                      
+            {                                   
+                if (i % 11 == 0)
+                { int a = i; int sum = 0;
+                    while (a > 0)
+                    {  qaliq = a % 10;
+                         sum = sum + qaliq;
+                           a = a / 10;
                     }
-                    while (h > 0)
-                    {
-                        z = h % 11;
-                        h = h - 11;
-
-                        if (z == 0)
+                    if (sum > 11)
+                    { count++;
+                        if (count == 11)
                         {
-                            state2 = true;
-                            break;
-                        }
-
-                    }
-
-                    if (state == true && state2==true )
-                    {
-                        count = 0;
-                        while (f > 0)
-                        {
-                            f = f + 11;
-                            count++;
-                            if (count++ == 11)
-                            {
-                                Console.WriteLine(i);
-                               
-                            }
-
-
-
-
+                            Console.WriteLine(i);
                         }
                     }
                 }
-               
             }
+        }
+    }
+}
+
+   
+    
+
+
+
+
+
+
+
+
           
 
 
@@ -87,7 +53,4 @@ namespace sual_4
 
 
 
-        }
-
-        }
-}
+  
